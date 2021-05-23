fd87344b215b436592ae5fbbf9673291a68661da
fwalker <francine@encantos.co>
2021-05-23 15:23:39 -0500

    Changed location of bookCover image

--- src/preview.js
+++ src/preview.js
@@ -61,7 +61,7 @@ export const Preview = observer(({ store }) => {
         className="preview-container"
         style={{ display: previewVisible ? "" : "none", position: "relative" }}
       >
-        <img src="./book-cover.jpeg" style={{ width: "300px" }} />
+        <img src="./public/book-cover.jpeg" style={{ width: "300px" }} />
         <img
           src={content}
           style={{
